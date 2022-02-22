98770ef0fcfb2124ef9190c6b9397716a1733a11
karlajaramillo <karlav.jaramillo@gmail.com>
2022-02-22 16:43:07 +0100

    Add cors and origin

--- server/config/middlewares.config.js
+++ server/config/middlewares.config.js
@@ -1,9 +1,13 @@
 const express = require("express");
+const cors = require("cors");
+
+const { ORIGIN } = process.env;
 
 function middlewares(app) {
   try {
     app.use(express.json());
     app.use(express.urlencoded({ extended: true }));
+    app.use(cors({origin: ORIGIN}));
   } catch (err) {
     console.log("error", err.message);
   }
